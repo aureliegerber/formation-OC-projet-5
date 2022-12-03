04d86b9d0e86b560a55077f4c67429cc9472643c
Aurelie Gerber <gerber.prof@gmail.com>
2022-12-03 16:19:57 +0100

    feat: display the products on the home page

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,5 @@
+let productItems = document.getElementById("items");
+
 fetch("http://localhost:3000/api/products")
     .then(function(res) {
         if (res.ok) {
@@ -5,7 +7,16 @@ fetch("http://localhost:3000/api/products")
         }
     })
     .then(function(value) {
-        console.log(value);
+        for (let i = 0; i < value.length; i++) {
+            productItems.innerHTML += `
+            <a href="./product.html?id=${value[i]._id}">
+                <article>
+                    <img src="${value[i].imageUrl}" alt="${value[i].altTxt}">
+                    <h3 class="productName">${value[i].name}</h3>
+                    <p class="productDescription">${value[i].description}</p>
+                </article>
+            </a>`;
+        }                       
     })
     .catch(function(err) {
         // Une erreur est survenue
